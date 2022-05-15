1759b9680aa61bdc85f1a9af8ce4ddf307153620
JuanFe <55063644+JuanFeA98@users.noreply.github.com>
2022-05-14 20:58:00 -0500

    First commit

---
+++ src/routes/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Login from '../containers/Login';
+import RecoveryPassword from '../containers/RecoveryPassword';
+
+import Home from '../pages/Home';
+import NotFound from '../pages/NotFound';
+
+import '../styles/global.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+          <Route exact path="/" element={<Home/>} />
+        <Route exact path="/login" element={<Login/>}/>
+        <Route exact path="/recovery-password" element={<RecoveryPassword/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
